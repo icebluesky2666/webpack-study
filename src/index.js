433b3b7e35e5a31d9eb2f77b27feb62a5fcbe7c2
jhqin <jiahao.qin@dragontrail.com>
2019-09-23 18:13:33 +0800

    webpack配置学习

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import './style.css';
+import './style.less';
 import Icon from './icon.jpg'
 import printMe from './print.js';
 function component() {
@@ -7,7 +7,7 @@ function component() {
   var btn = document.createElement('button');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['你好', '工程化'], ' ');
+  element.innerHTML = _.join(['你好', '工程化,heiheihei!'], ' ');
   element.classList.add('hello');
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
@@ -18,7 +18,19 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+  
+  //es6
+  var x = [1,2,3,4,5,6,7,8];
+  x.forEach((value,index)=>{
+    console.log(value)
+  })
   return element;
 }
 
 document.body.appendChild(component());
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,150 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    apps: './src/index.js',
-    print: './src/print.js'
+    //apps: './src/index.js',
+    //print: './src/print.js'
+    app: './src/index.js'
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name]-[hash].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module:{
-    rules:[
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  resolve: {
+    alias: {
+      vue$: path.resolve(__dirname, 'src/lib/vue/dist/vue.esm.js'),
+      '@': path.resolve(__dirname, 'src/')
+    },
+    //extensions: [".js", ".vue",".json"],
+    /* externals: {
+      jquery: 'jQuery'
+    }, */
+  },
+  target: 'web',
+  devtool: 'inline-source-map',
+  mode: "production",
+  module: {
+    rules: [
       {
-        test:/\.css$/,
-        use:[
-          'style-loader',
+        test: /\.js$/,
+        exclude: /(node_modules)/,  // 加快编译速度，不包含node_modules文件夹内容
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
           'css-loader'
         ]
       },
       {
-        test:/\.(png|jpg|gif|svg)/,
-        use:[
-          'file-loader'
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader, // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpeg|ico)$/,
+        use: [
+          //'file-loader',
+          {
+            loader: 'url-loader', // 根据图片大小，把图片优化成base64
+            options: {
+              limit: 10000
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              }
+            }
+          },
         ]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use:[
+        use: [
           'file-loader'
         ]
       }
     ]
   },
-  plugins:[
+  plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
-  ]
+      title: 'webpack4',
+      //filename: 'main.html',
+      //template: path.resolve(__dirname, 'src/index.html'),
+      /* minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeAttributeQuotes: true // 移除属性的引号
+      } */
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css', // 设置最终输出的文件名
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+    }),
+    new BundleAnalyzerPlugin(
+      {
+        analyzerMode: 'server',
+        analyzerHost: '127.0.0.1',
+        analyzerPort: 8889,
+        reportFilename: 'report.html',
+        defaultSizes: 'parsed',
+        openAnalyzer: true,
+        generateStatsFile: false,
+        statsFilename: 'stats.json',
+        statsOptions: null,
+        logLevel: 'info'
+      }
+    ),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({}),
+      /* new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }), */
+    ]
+  }
 };
